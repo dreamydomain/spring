a85913d8b26cc3fcf6efcb1428ccc51e16d2b2b7
horhou <horace.hou@leandev.se>
2020-08-05 17:58:47 +0800

    2020年8月5日17:58:44

--- src/main/java/com/horace/spring/Application.java
+++ src/main/java/com/horace/spring/Application.java
@@ -2,8 +2,10 @@ package com.horace.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 使用MongoDB启动报错：Failed to determine a suitable driver class，增加以下参数
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
