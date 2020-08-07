1fe0c2ae9aa66b9b5aa2c43dc7533179cc194606
horhou <horace.hou@leandev.se>
2020-08-07 17:59:27 +0800

    2020年8月7日17:59:24

--- src/main/java/com/horace/spring/zoo/MongoDBZoo.java
+++ src/main/java/com/horace/spring/zoo/MongoDBZoo.java
@@ -5,14 +5,13 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoDBZoo {
+
     public static void main(String[] args) {
-
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://horace:Super123@cluster0.zrgqp.azure.mongodb.net/spring?retryWrites=true&w=majority");
-
+        MongoClientURI uri = new MongoClientURI("mongodb+srv://horace:Super123@cluster0.zrgqp.azure.mongodb.net/spring?retryWrites=true&w=majority");
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase database = mongoClient.getDatabase("spring");
         database.createCollection("testCollection");
         mongoClient.close();
     }
+
 }
