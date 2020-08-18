59e953fee6fbe1a160d231c7a830504d9f61c15e
horhou <horace.hou@leandev.se>
2020-08-18 15:25:00 +0800

    2020年8月18日15:24:46 koss test g1 and g3 database.

--- src/main/demo/KOSSTest1.java
+++ src/main/demo/KOSSTest1.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.util.Strings;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -5,31 +7,87 @@ import java.sql.Statement;
 
 public class KOSSTest1 {
     public static void main(String[] args) throws Exception {
-        String url = "jdbc:postgresql://c7-db2.koss.leandev.cn:5432/";
-        String userName = "postgres";
-        String password = "postgres";
-        String sql = "select * from (select userid,company_id,count(1) as count_num from ManagedCompanySetting GROUP BY userid,company_id) as tb where count_num > 1";
-        for (int i = 1; i <= 400; i++) {
-            String db = "koss" + i;
-            test1(url, db, userName, password, sql);
-        }
+        testG1();
     }
 
-    private static void test1(String url, String db, String userName, String password, String sql) throws Exception {
+    private static void testG1() throws Exception {
         Class.forName("org.postgresql.Driver");
-        Connection conn = DriverManager.getConnection(url + db, userName, password);
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://c7-db.koss.leandev.cn:5432/kossadmin", "postgres", "postgres");
         Statement state = conn.createStatement();
-        ResultSet rs = state.executeQuery(sql);
-        System.out.println(db);
+        ResultSet rs = state.executeQuery("select id from users where serverinstance = 'g1'");
+        StringBuilder inIds = new StringBuilder(Strings.EMPTY);
         while (rs.next()) {
-            long col01 = rs.getLong(1);
-            long col02 = rs.getLong(2);
-            long col03 = rs.getLong(3);
-            System.out.println(col01 + " | " + col02 + " | " + col03);
+            inIds.append(rs.getLong(1)).append(",");
         }
-        System.out.println("______________________________________________________");
+        String inSql = inIds.substring(0, inIds.length() - 1);
         rs.close();
         state.close();
         conn.close();
+
+        String sql2 = "select * from (select userid,company_id,count(1) as count_num from ManagedCompanySetting GROUP BY userid,company_id) as tb where count_num > 1 and userid in (" + inSql + ")";
+        for (int i = 1; i <= 400; i++) {
+            String db = "koss" + i;
+            Connection connDB2 = DriverManager.getConnection("jdbc:postgresql://c7-db.koss.leandev.cn:5432/" + db, "postgres", "postgres");
+            Statement state2 = connDB2.createStatement();
+            ResultSet rs2 = state2.executeQuery(sql2);
+            int resultIndex = 0;
+            while (rs2.next()) {
+                if (resultIndex == 0) {
+                    System.out.println("______________________________________________________");
+                    System.out.println(db);
+                    System.out.println("userid             | companyid          | num");
+                }
+                long col01 = rs2.getLong(1);
+                long col02 = rs2.getLong(2);
+                long col03 = rs2.getLong(3);
+                System.out.println(col01 + " | " + col02 + " | " + col03);
+                resultIndex++;
+            }
+            rs2.close();
+            state2.close();
+            connDB2.close();
+        }
     }
+
+    private static void testG3() throws Exception {
+        Class.forName("org.postgresql.Driver");
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://c7-db.koss.leandev.cn:5432/kossadmin", "postgres", "postgres");
+        Statement state = conn.createStatement();
+        ResultSet rs = state.executeQuery("select id from users where serverinstance = 'g3'");
+        StringBuilder inIds = new StringBuilder(Strings.EMPTY);
+        while (rs.next()) {
+            inIds.append(rs.getLong(1)).append(",");
+        }
+        String inSql = inIds.substring(0, inIds.length() - 1);
+        rs.close();
+        state.close();
+        conn.close();
+
+        String sql2 = "select * from (select userid,company_id,count(1) as count_num from ManagedCompanySetting GROUP BY userid,company_id) as tb where count_num > 1 and userid in (" + inSql + ")";
+        for (int i = 201; i <= 400; i++) {
+            String db = "koss" + i;
+            Connection connDB2 = DriverManager.getConnection("jdbc:postgresql://c7-db2.koss.leandev.cn:5432/" + db, "postgres", "postgres");
+            Statement state2 = connDB2.createStatement();
+            ResultSet rs2 = state2.executeQuery(sql2);
+            int resultIndex = 0;
+            while (rs2.next()) {
+                if (resultIndex == 0) {
+                    System.out.println("______________________________________________________");
+                    System.out.println(db);
+                    System.out.println("userid             | companyid          | num");
+                }
+                long col01 = rs2.getLong(1);
+                long col02 = rs2.getLong(2);
+                long col03 = rs2.getLong(3);
+                System.out.println(col01 + " | " + col02 + " | " + col03);
+                resultIndex++;
+            }
+            rs2.close();
+            state2.close();
+            connDB2.close();
+        }
+    }
+
 }
