c099bceb38f5676a1e6ed8f2bc6eb3266fd88e4b
horhou <horace.hou@leandev.se>
2020-09-02 10:26:01 +0800

    G2

--- src/main/demo/KOSSTest1.java
+++ src/main/demo/KOSSTest1.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class KOSSTest1 {
     public static void main(String[] args) throws Exception {
-        testG1();
+        testG2();
     }
 
     private static void testG1() throws Exception {
@@ -70,6 +70,41 @@ public class KOSSTest1 {
             state2.close();
             connDB2.close();
         }
+    }
+
+    private static void testG2() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String sql2 = "select userid,company_id,count(1) as count_num from ManagedCompanySetting GROUP BY userid,company_id having count(1) > 1";
+        Connection connDB2 = DriverManager.getConnection("jdbc:postgresql://c7-db2.koss.leandev.cn:5432/koss1_os", "postgres", "postgres");
+        Statement state2 = connDB2.createStatement();
+        ResultSet rs2 = state2.executeQuery(sql2);
+        String inSettingIds = "";
+        while (rs2.next()) {
+            long col01 = rs2.getLong(1);
+            long col02 = rs2.getLong(2);
+            long col03 = rs2.getLong(3);
+            System.out.println(col01 + " | " + col02 + " | " + col03);
+            Statement stateIds = connDB2.createStatement();
+            ResultSet ids = stateIds.executeQuery("select id from ManagedCompanySetting where userid = " + col01 + " and company_id = " + col02);
+            int idsInt = 0;
+            while (ids.next()) {
+                if (idsInt > 0) {
+                    inSettingIds += (ids.getLong(1) + ",");
+                }
+                idsInt++;
+            }
+            ids.close();
+            stateIds.close();
+        }
+        if (StringUtil.isNotNullOrEmpty(inSettingIds) && inSettingIds.endsWith(",")) {
+            inSettingIds = inSettingIds.substring(0, inSettingIds.length() - 1);
+            String delSql = "delete from ManagedCompanySetting where id in (" + inSettingIds + ")";
+            System.out.println(delSql);
+            connDB2.createStatement().execute(delSql);
+        }
+        rs2.close();
+        state2.close();
+        connDB2.close();
     }
 
     private static void testG3() throws Exception {
