4d8b3d02bde88b15313e962c073106fd75a76dc1
horhou <horace.hou@leandev.se>
2020-09-07 13:38:56 +0800

    Bill

--- src/main/java/com/horace/spring/controller/BillController.java
+++ src/main/java/com/horace/spring/controller/BillController.java
@@ -5,6 +5,7 @@ import com.horace.spring.model.mongo.Bill;
 import com.horace.spring.model.mongo.ResponseDto;
 import com.horace.spring.repository.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class BillController {
     @Autowired
     private BillRepository billRepository;
 
+    @RequestMapping("bill/getAllBills")
     public ResponseDto getAllBills() {
         List<Bill> entity = this.billRepository.findAll();
         return new ResponseDto(entity, "", true, ErrorType.SUCCESS);
